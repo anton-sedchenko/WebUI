e4f5097f14d86b3887e5c926275ce82b0a90aa21
Anton Sedchenko <anton.sedchenko@gmail.com>
2021-04-20 13:45:54 +0300

    HW_4 task_2

--- HW 4/js/task_2.js
+++ HW 4/js/task_2.js
@@ -1,1 +1,24 @@
+'use strict';
+ 
+function morseDecoder(str) {
+	const morseCode = morseAlphabet;
+	const messageWordsArr = str.split('   ');
+	let result = [];
 
+	function wordDecoder(word) {
+		let wordCharsArr = word.split(' ');
+
+		const decodedWordsArr = wordCharsArr.map(item => {
+			for (let char in morseCode) {
+				if (morseCode[char] === item) {
+					return char.toUpperCase();
+				}
+			}
+
+		});
+		return decodedWordsArr.join('');
+	}
+
+	result = messageWordsArr.map(word => wordDecoder(word));
+	return result.join(' ');
+}
