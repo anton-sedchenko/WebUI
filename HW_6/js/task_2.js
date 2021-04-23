2c4888ff2c089c72f74c8d22b9b1e728b834ddd6
Anton Sedchenko <anton.sedchenko@gmail.com>
2021-04-23 13:23:12 +0300

    HW_6 task_2

--- HW_6/js/task_2.js
+++ HW_6/js/task_2.js
@@ -1,2 +1,18 @@
 'use strict';
 
+class Dictionary {
+	constructor() {
+		this.dictionary = {}
+	}
+
+	newEntry(word, descr) {
+		this.dictionary[word] = descr;
+	}
+
+	look(word) {
+		if (this.dictionary[word]) {
+			return this.dictionary[word];
+		}
+		return `Can't find entry for ${word}`;
+	}
+}
